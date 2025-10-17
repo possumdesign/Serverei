2e82b76d4997422a9f15982acfb488a4085ba800
possumdesign <whiteviper82@live.com>
2025-10-17 12:45:49 +0200

    Rewrite, neuer Ansatz, 2ter Versuch

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using server;
+internal class Program
+{
+    
+    public static void Main(string[] args)
+    {
+    //MUSSWEG    SpeichernLaden.Laden(ref tagebuch, ref todos, ref einkauf);
+    //MUSSWEG    UhrThread = new Thread(Uhr.AnzeigeUhr);
+        Menu();
+    }
+    public static void Menu()
+    {
+        int auswahl = 0;
+        List<string> options = new List<string>()
+        {
+            "1. Tagebucheintrag erstellen",
+            "2. To-Do-Eintrag erstellen",
+            "3. Einkaufsliste erstellen",
+            "4. Tagebucheinträge anzeigen",
+            "5. To-Do-Einträge anzeigen",
+            "6. Einkaufslisten anzeigen",
+        };
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n");
+            for (int i = 0; i < options.Count; i++)
+            {
+                if(i == auswahl)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine(options[i]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(options[i]);
+                }
+            }
+            if(!UhrThread.IsAlive) UhrThread.Start();
+
+            var key = Console.ReadKey().Key;
+            if (key == ConsoleKey.UpArrow) auswahl--;
+            else if (key == ConsoleKey.DownArrow) auswahl++;
+            if (auswahl < 0) auswahl = options.Count - 1;
+            if (auswahl >= options.Count) auswahl = 0;
+            if(key == ConsoleKey.Enter)
+            {
+                if(auswahl == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine("Geben Sie den Titel des Tagebucheintrags ein: ");
+                    string titel = Console.ReadLine() ?? "";
+                    if(titel.Trim() == "") titel = "Ohne Titel";
+                    var tagebucheintrag = new TagebuchEintrag(titel);
+                    tagebuch.Add(tagebucheintrag);
+                    SpeichernLaden.Speichern(tagebuch, todos, einkauf);
+
+                }
+                else if(auswahl == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine("Geben Sie denTitel des To-Do eintrags ein: ");
+                    string titel = Console.ReadLine() ?? "";
+                    if (titel.Trim() == "") titel = "Ohne Titel";
+
+                    int auswahl2 = 0;
+                    List<string> options2 = new List<string>()
+                    {
+                        "1. Mit Erinnerung",
+                        "2. Ohne Erinnerung"
+                    };
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\n\n\n\n");
+                        Console.WriteLine("Soll eine Erinnerung gesetzt werden? ");
+                        for (int i = 0; i < options2.Count; i++)
+                        {
+                            if (i == auswahl2)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                Console.BackgroundColor = ConsoleColor.White;
+                                Console.WriteLine(options2[i]);
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.WriteLine(options2[i]);
+                            }
+                        }
+                        var key2 = Console.ReadKey().Key;
+                        if (key2 == ConsoleKey.UpArrow) auswahl2--;
+                        else if (key2 == ConsoleKey.DownArrow) auswahl2++;
+                        if (auswahl2 < 0) auswahl2 = options2.Count - 1;
+                        if (auswahl2 >= options2.Count) auswahl2 = 0;
+                        if (key2 == ConsoleKey.Enter) break;
+                    }
+                    if(auswahl2 == 0)
+                    {
+                        var todoeintrag = new ToDoEintrag(titel, ToDoEintrag.eingabeErinnerungsdatum());
+                        todos.Add(todoeintrag);
+
+                    }
+                    else
+                    {
+                        var todoeintrag = new ToDoEintrag(titel);
+                        todos.Add(todoeintrag);
+                    }
+                    SpeichernLaden.Speichern(tagebuch, todos, einkauf);
+                }
+                else if(auswahl == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine("Geben Sie den Titel der Einkaufsliste ein: ");
+                    string titel = Console.ReadLine() ?? "";
+                    if (titel.Trim() == "") titel = "Ohne Titel";
+                    var liste = new Einkaufsliste(titel);
+                    einkauf.Add(liste);
+                    SpeichernLaden.Speichern(tagebuch, todos, einkauf);
+                }
+                else if(auswahl == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine("Tagebucheinträge\n\n");
+                    foreach (var eintrag in tagebuch)
+                    {
+                        eintrag.Anzeigen();
+                    }
+                    Console.ReadKey();
+                }
+                else if (auswahl == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine("To-Do Notizen\n\n");
+                    foreach (var eintrag in todos)
+                    {
+                        eintrag.Anzeigen();
+                    }
+                    Console.ReadKey();
+                }
+                else if (auswahl == 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine("Einkaufslisten\n\n");
+                    foreach (var eintrag in einkauf)
+                    {
+                        eintrag.Anzeigen();
+                    }
+                    Console.ReadKey();
+                }
+
+
+            }
+        }
+    }
+}
--- server19.cs
+++ server19.cs
@@ -5,77 +5,73 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace Serverei
+internal abstract class BaseServer
 {
-    internal class server19
-    {
-        public int HEunit { get; set; }
-        public string Hardware { get; set; }
-        public int Fan12 { get; set; }
-        public int Fan6 { get; set; }  
-        public int Fan4 { get; set; }   
+    public Guid Id = Guid.NewGuid();
+    public string Typ;
+    public int HeightU;      // 1..4
+    public int FanMm;        // 40/60/80/120
+    public int Netzteile;    // 1 bis 3
+    public int SSD;          // 0 bis 4
+    public int HDD;          // 0bis 24
+    public int GPU;          // 0 bis 8
+    public string Origin;    // "b" = Base (admin), "c" = Custom (User)
 
-        public void Anzeigen()
-        {
-            Console.WriteLine($"Typ: {HEunit} mit {Hardware}");
-        }
-    }
-    internal class HE4 : server19 
+    public BaseServer() { }
+    public BaseServer(string typ, int heightU, int fanMm, int netzteile, int ssd, int hdd, int gpu, string origin)
     {
-        [JsonConstructor]
-        public HE4(int HEunit, string Hardware)
-        {
-            this.HEunit = HEunit;
-            this.Hardware = Hardware;
-        }
-
-        public HE4(int HEunit)
-        {
-            HEunit = 4;
-        }
-        public int Fan12 = 6;
-    }
-    internal class HE3 : server19
-    {
-        [JsonConstructor]
-        public HE3(int HEunit, string Hardware)
-        {
-            this.HEunit = HEunit;
-            this.Hardware = Hardware;
-        }
-        public HE3(int HEunit)
-        {
-            HEunit = 3;
-        }
-        public int Fan6 = 5;
+        Typ = typ; HeightU = heightU; FanMm = fanMm; Netzteile = netzteile; SSD = ssd; HDD = hdd; GPU = gpu; Origin = origin;
     }
 
-    internal class HE2 : server19
+    public string FanLabel()
     {
-        [JsonConstructor]
-        public HE2(int HEunit, string Hardware)
-        {
-            this.HEunit = HEunit;
-            this.Hardware = Hardware;
-        }
-        public HE2(int HEunit)
-        {
-            HEunit = 2;
-        }
-        public int Fan6 = 4;
+        return "fan" + FanMm + "mm";
     }
-    internal class HE1 : server19
+
+    public abstract BaseServer Clone();
+}
+
+internal class He1 : BaseServer
+{
+    public He1() { }
+    public He1(string typ, int fanMm, int netzteile, int ssd, int hdd, int gpu, string origin)
+        : base(typ, 1, fanMm, netzteile, ssd, hdd, gpu, origin) { }
+    public override BaseServer Clone()
     {
-        [JsonConstructor]
-        public HE1(int HEunit, string Hardware)
-        {
-            this.HEunit = HEunit;
-            this.Hardware = Hardware;
-        }
-        public HE1(int HEunit)
-        {
-            HEunit = 1;
-        }
-        public int Fan4 = 5;
+        return new He1(Typ, FanMm, Netzteile, SSD, HDD, GPU, Origin);
     }
 }
+
+internal class He2 : BaseServer
+{
+    public He2() { }
+    public He2(string typ, int fanMm, int netzteile, int ssd, int hdd, int gpu, string origin)
+        : base(typ, 2, fanMm, netzteile, ssd, hdd, gpu, origin) { }
+    public override BaseServer Clone()
+    {
+        return new He2(Typ, FanMm, Netzteile, SSD, HDD, GPU, Origin);
+    }
+}
+
+internal class He3 : BaseServer
+{
+    public He3() { }
+    public He3(string typ, int fanMm, int netzteile, int ssd, int hdd, int gpu, string origin)
+        : base(typ, 3, fanMm, netzteile, ssd, hdd, gpu, origin) { }
+    public override BaseServer Clone()
+    {
+        return new He3(Typ, FanMm, Netzteile, SSD, HDD, GPU, Origin);
+    }
+}
+
+internal class He4 : BaseServer
+{
+    public He4() { }
+    public He4(string typ, int fanMm, int netzteile, int ssd, int hdd, int gpu, string origin)
+        : base(typ, 4, fanMm, netzteile, ssd, hdd, gpu, origin) { }
+    public override BaseServer Clone()
+    {
+        return new He4(Typ, FanMm, Netzteile, SSD, HDD, GPU, Origin);
+    }
+}
+//wurde öfter autovervollständigt,zweiter versuch, Proof of concept
